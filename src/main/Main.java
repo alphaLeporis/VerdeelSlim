caa69e65bb41858e0942250d10c1ef20c8c28eb9
⍺ Leporis <louis.delooze@gmail.com>
2021-11-25 14:10:42 +0100

    Toevoegen main

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Main main = new Main();
+        main.run();
+    }
+
+    public Main()
+    {
+
+    }
+
+    public void run()
+    {
+
+    }
+}
