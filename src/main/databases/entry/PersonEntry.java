db03ba6c7cefa2401ac6fe9a39ca31169e4e631b
amichielsen <83591375+amichielsen@users.noreply.github.com>
2021-12-02 14:42:56 +0100

    PersonEntry aanpassing

--- src/main/databases/entry/PersonEntry.java
+++ src/main/databases/entry/PersonEntry.java
@@ -1,7 +1,28 @@
 package databases.entry;
 
 public class PersonEntry extends DatabaseEntry {
+    private double amountPaid;
+    private double amountBorrowed;
+
     public PersonEntry(String name) {
         super(name);
+        this.amountBorrowed = 0;
+        this.amountPaid = 0;
+    }
+
+    public double getAmountPaid() {
+        return amountPaid;
+    }
+
+    public void setAmountPaid(double amountPaid) {
+        this.amountPaid = amountPaid;
+    }
+
+    public double getAmountBorrowed() {
+        return amountBorrowed;
+    }
+
+    public void setAmountBorrowed(double amountBorrowed) {
+        this.amountBorrowed = amountBorrowed;
     }
 }
