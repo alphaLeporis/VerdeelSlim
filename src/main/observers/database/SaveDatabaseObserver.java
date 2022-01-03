24d565d1cdf904f3a6f6615bf809d27f9ec1c894
⍺ Leporis <louis.delooze@gmail.com>
2022-01-03 09:27:16 +0100

    - Save handler

--- src/main/observers/database/SaveDatabaseObserver.java
+++ src/main/observers/database/SaveDatabaseObserver.java
@@ -3,9 +3,10 @@ package observers.database;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+// Can be implemented to save to a database or csv file handler...
 public class SaveDatabaseObserver implements PropertyChangeListener {
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        System.out.println("Pim Padaboem observeridoo");
+        //System.out.println(evt.getNewValue());
     }
 }
